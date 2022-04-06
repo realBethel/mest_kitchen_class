98dc2bc8a141b3966f8c25c34e2e00c6cb068720
King Bethel <bethelmelariri@gmail.com>
2022-04-06 10:16:12 +0000

    upload code

---
+++ mest-kitchen.py
@@ -0,0 +1,19 @@
+class User():
+    first_name = ''
+    last_name = ''
+    email_address = ''
+    phone_number = ''
+    allergies = ''
+    food_diet = ''
+    user_type = ''
+
+class MealItem():
+    name = ''
+    description = ''
+    serving_size = ''
+    allergens = ''
+
+class Order():
+    meal = ''
+    portion = ''
+    
